584f0a8cbe6fa825dd053b244fee62fdb8243b27
love1my <shuimeiren2022@163.com>
2025-09-02 18:58:39 +0800

    9-2update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
-
+import preact from '@astrojs/preact';
 export default defineConfig({
-  site: "https://zeaquea.netlify.app/"
+  site: "https://zeaquea.netlify.app/",
+  integrations: [preact()],
 });
